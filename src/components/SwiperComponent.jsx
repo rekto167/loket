12d1a36e372b9f35dffe6ac1b6f936ad9827dad7
Renal Eki Riyanto <renalekiriyanto@gmail.com>
2022-01-09 07:24:29 +0700

    swiper component event recommended

---
+++ src/components/SwiperComponent.jsx
@@ -0,0 +1,48 @@
+import dunia from '../assets/dunia-mencekam.jpg'
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import CardItem from './CardItem'
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation"
+
+// import "./styles.css";
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Navigation
+} from 'swiper';
+
+// install Swiper modules
+SwiperCore.use([Navigation]);
+
+
+const SwiperComponent = () => {
+    return(
+            <Swiper slidesPerView={3} spaceBetween={30} navigation={true} className="mySwiper">
+                 <SwiperSlide>
+                     <CardItem />
+                 </SwiperSlide>
+                 <SwiperSlide>
+                     <CardItem />
+                 </SwiperSlide>
+                 <SwiperSlide>
+                     <CardItem />
+                 </SwiperSlide>
+                 <SwiperSlide>
+                     <CardItem />
+                 </SwiperSlide>
+                 <SwiperSlide>
+                     <CardItem />
+                 </SwiperSlide>
+                 <SwiperSlide>
+                     <CardItem />
+                 </SwiperSlide>
+            </Swiper>
+    )
+}
+
+export default SwiperComponent

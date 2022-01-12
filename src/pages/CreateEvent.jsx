82fe24d0a72d601b5ccb0f625b00a9cf387bfaee
Renal Eki Riyanto <renalekiriyanto@gmail.com>
2022-01-12 17:03:54 +0700

    ui for deskription event in create event page

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -1,11 +1,67 @@
+// library
 import Navbar from '../components/white/Navbar'
 import Container from '../components/Container'
-import bannerEvent from '../assets/banner-event.jpg'
-import * as React from 'react';
+import {useState} from 'react';
+// Material UI
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+// CKEditor
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// assets
+import bannerEvent from '../assets/banner-event.jpg'
+// styles
+import './CreateEvent.css'
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 2 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+
+
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
 
 const CreateEvent = () => {
+    const [value, setValue] = useState(0);
+    const [showSyarat, setShowSyarat] = useState(false);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const handleShowSyarat = () => {
+        setShowSyarat(!showSyarat);
+    }
+
     return(
         <>
             <Navbar />
@@ -37,7 +93,7 @@ const CreateEvent = () => {
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
                                         </svg>
                                     </button>
-                                    <input type="text" placeholder="Nama Organizer" className="focus:placeholder:text-transparent " />
+                                    <input type="text" placeholder="Nama Organizer" className="focus:placeholder:text-transparent outline-0" />
                                 </div>
                             </div>
                             <div className="flex flex-col mt-5">
@@ -74,8 +130,74 @@ const CreateEvent = () => {
                     </div>
             </Container>
 
-            <Container>
+            <Container width="w-[70%] shadow-lg border border-gray-300 h-50 m-auto mt-20 flex flex-col">
+                <div className="w-full mx-auto">
+                    <Box sx={{ width: '100%' }}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider', widht: '100%' }}>
+                            <Tabs value={value} onChange={handleChange} sx={{ width:'100%' }} centered >
+                                <Tab label="Kategori Tiket" {...a11yProps(0)}  sx={{ width:'100%' }} />
+                                <Tab label="Deskripsi Event" {...a11yProps(1)} sx={{ width:'100%' }} />
+                            </Tabs>
+                        </Box>
+                        <TabPanel value={value} index={0}>
+                            Kategori Tiket
+                        </TabPanel>
+                        <TabPanel value={value} index={1}>
+                            <CKEditor
+                                editor={ ClassicEditor }
+                                data=""
+                                onReady={ editor => {
+                                    // You can store the "editor" and use when it is needed.
+                                    console.log( 'Editor is ready to use!', editor );
+                                } }
+                                onChange={ ( event, editor ) => {
+                                    const data = editor.getData();
+                                    console.log( { event, editor, data } );
+                                } }
+                                onBlur={ ( event, editor ) => {
+                                    console.log( 'Blur.', editor );
+                                } }
+                                onFocus={ ( event, editor ) => {
+                                    console.log( 'Focus.', editor );
+                                } }
+                            />
+                            <button className="text-gray-500 hover:text-orange-500 flex mt-5 mb-5" onClick={handleShowSyarat} >
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                {showSyarat ?
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12H9m12 0a9 9 0   11-18 0 9 9 0 0118 0z" />
+                                    :
 
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                }
+                                </svg>
+                                <p>Syarat & Ketentuan</p>
+                            </button>
+
+                            <div className={showSyarat ? 'block' : 'hidden'}>
+                                <CKEditor
+                                    editor={ ClassicEditor }
+                                    data=""
+                                    onReady={ editor => {
+                                        // You can store the "editor" and use when it is needed.
+                                        console.log( 'Editor is ready to use!', editor );
+                                    } }
+                                    onChange={ ( event, editor ) => {
+                                        const data = editor.getData();
+                                        console.log( { event, editor, data } );
+                                    } }
+                                    onBlur={ ( event, editor ) => {
+                                        console.log( 'Blur.', editor );
+                                    } }
+                                    onFocus={ ( event, editor ) => {
+                                        console.log( 'Focus.', editor );
+                                    } }
+                                />
+                            </div>
+
+
+                        </TabPanel>
+                    </Box>
+                </div>
             </Container>
         </>
     )
